a9c324c94c0d3fd7c179695667bfc0727048b1e9
machadogustavors <machado.gustavo2905@gmail.com>
2025-06-22 00:32:09 -0300

    in work login

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,30 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 import { Login } from '@src/pages/Login'
 import { Register } from '@src/pages/Register'
+import { ConfirmAccount } from '@src/pages/ConfirmAccount'
 import { AuthGuard } from '@src/components/AuthGuard'
 import { AuthLayout } from '@src/components/AuthLayout'
+import { useAuth } from '@src/contexts/AuthContext'
+
+function HomeRedirect() {
+  const { isAuthenticated } = useAuth();
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" replace />;
+  }
+  return <Navigate to="/login" replace />;
+}
 
 export function Router() {  
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomeRedirect />} />
         {/* Public routes */}
         <Route element={<AuthGuard isPrivate={false} />}>
           <Route element={<AuthLayout />}>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/confirm-account" element={<ConfirmAccount />} />
           </Route>
         </Route>
 
